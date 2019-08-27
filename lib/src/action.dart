bf3cc11bf1804ed9703102034456bd33c97cdc60
LongYinan <lynweklm@gmail.com>
2019-08-27 17:40:18 +0800

    feat(action): add noop action

--- lib/src/action.dart
+++ lib/src/action.dart
@@ -2,7 +2,9 @@ const SetStateSymbol = '__SET_STATE__';
 const DispatchSymbol = '__DISPATCH__';
 
 class Action<T> {
-  Action(this.type, this.payload);
+  const Action(this.type, this.payload);
   final String type;
   final T payload;
 }
+
+const noopAction = const Action('__noop__', null);
